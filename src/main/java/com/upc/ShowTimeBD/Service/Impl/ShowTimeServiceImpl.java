ef61368920f2b3b674c9ef9bbead6a48a01cf5ea
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2023-11-03 00:48:54 -0500

    Updated

--- src/main/java/com/upc/ShowTimeBD/Service/Impl/ShowTimeServiceImpl.java
+++ src/main/java/com/upc/ShowTimeBD/Service/Impl/ShowTimeServiceImpl.java
@@ -9,15 +9,19 @@ import com.upc.ShowTimeBD.Shared.CinemaResponse;
 import com.upc.ShowTimeBD.Shared.FilmResponse;
 import feign.FeignException;
 import jakarta.validation.ValidationException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class ShowTimeServiceImpl implements ShowTimeService {
     private final ShowTimeRepository showTimeRepository;
 
@@ -25,14 +29,12 @@ public class ShowTimeServiceImpl implements ShowTimeService {
     private CinemaClient cinemaClient;
     @Autowired
     private MovieClient movieClient;
-    public ShowTimeServiceImpl(ShowTimeRepository showTimeRepository) {
-        this.showTimeRepository = showTimeRepository;
-    }
 
     @Override
     @Transactional
     public ShowTimeModel save(ShowTimeModel showTimeModel) throws Exception {
         String id = showTimeModel.getCinemaId().toString();
+        ValidateIfCinemaExists(id);
         ValidateIfMovieExist(showTimeModel.getMovieId().toString());
         return showTimeRepository.save(showTimeModel);
     }
@@ -51,23 +53,24 @@ public class ShowTimeServiceImpl implements ShowTimeService {
     public Optional<ShowTimeModel> getById(Long id) throws Exception {
         return showTimeRepository.findById(id);
     }
-    @Override
-    public void ValidateIfCinemaExists(String id) throws Exception {
-     try{
-         ResponseEntity<CinemaResponse> CineClubResponse = cinemaClient.getCinemaByName(Long.valueOf(id));
-            if(CineClubResponse.getBody().getId() == null){
-                throw new ValidationException("Cinema does not exist");
-            }
 
-     } catch (FeignException feignException) {
-         throw new ValidationException(feignException.getMessage());
-     }
-    }
+
     private void ValidateIfMovieExist(String id) throws Exception{
         try{
             ResponseEntity<FilmResponse> FilmbResponse = movieClient.getMovieById(Long.valueOf(id));
             if(FilmbResponse.getBody().getId() == null){
                 throw new ValidationException("Film does not exist");
+            }
+
+        } catch (FeignException feignException) {
+            throw new ValidationException(feignException.getMessage());
+        }
+    }
+    private void ValidateIfCinemaExists(String id) throws Exception {
+        try{
+            ResponseEntity<CinemaResponse> CineClubResponse = cinemaClient.getCinemaByName(Long.valueOf(id));
+            if(CineClubResponse.getBody().getId() == null){
+                throw new ValidationException("Cinema does not exist");
             }
 
         } catch (FeignException feignException) {
--- src/main/java/com/upc/ShowTimeBD/ShowTimeBdApplication.java
+++ src/main/java/com/upc/ShowTimeBD/ShowTimeBdApplication.java
@@ -2,8 +2,10 @@ package com.upc.ShowTimeBD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ShowTimeBdApplication {
 
 	public static void main(String[] args) {
